d643a08468634440b49fbc23955086ccdf59c79f
bey <betenatefera@gmail.com>
2021-11-16 22:09:32 +0300

    modified

--- 0x01-python-if_else_loops_functions/3-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabet.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if chr(i) != 'q' and chr(i) != 'e':
-        print('{}'.format(chr(i)), end='')
+for letter in range(97, 123):
+    if letter == 101 or letter == 113:
+        continue
+    else:
+        print("{}".format(chr(letter)), end="")
