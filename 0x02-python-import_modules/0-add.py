cb99c7e631e0720f42306ba9f98f7b24b26ce5fd
bey <betenatefera@gmail.com>
2021-11-19 12:06:51 +0300

    pushed task 1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-def add (a, b):
-     return a + b
-print("{} + {} = {}".format(a,b,add(a, b)))
-
-
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
