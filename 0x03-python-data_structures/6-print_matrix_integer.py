6637ad72aa30cbf319696f671ec765fb44a0c98f
bey <betenatefera@gmail.com>
2021-11-22 22:23:01 +0300

    modiry lines of code

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        clm = 1
-        for clm in row:
-            if clm == len(row):
-                print("{:d}".format(clm), end="")
+    for i in matrix:
+        l = 1
+        for j in i:
+            if l == len(i):
+                print("{:d}".format(j), end="")
             else:
-                print("{:d}".format(clm), end=" ")
-            clm = clm + 1
+                print("{:d}".format(j), end=" ")
+            l = l + 1
         print()
