9bf6bf5fdd3f22868e589efb2af4727ec8ae937e
bey <betenatefera@gmail.com>
2021-11-22 21:36:33 +0300

    correct name

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,11 @@
-
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        clm = 1
+        for clm in row:
+            if clm == len(row):
+                print("{:d}".format(clm), end="")
+            else:
+                print("{:d}".format(j), end="")
+            clm = clm + 1
+        print()
