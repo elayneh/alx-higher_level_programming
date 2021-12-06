d60983314e7aef4d2dd83a7e80b9e15da44c3892
bey <betenatefera@gmail.com>
2021-12-06 19:36:09 +0300

    push task 1

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+def safe_print_list(my_list=[], x=0):
+    try:
+        for index in range(0, x):
+            print(my_list[index], end="")
+        print("")
+        return (x)
+    except:
+        print("")
+        return(index)
