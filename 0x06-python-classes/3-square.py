8c68050eb202147d40349865604dc6b82674522b
bey <betenatefera@gmail.com>
2021-12-08 00:11:21 +0300

    modified

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,6 +5,7 @@
 class Square:
     '''Class Square '''
     def __init__(self, size=0):
+        ''' Constructor'''
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
