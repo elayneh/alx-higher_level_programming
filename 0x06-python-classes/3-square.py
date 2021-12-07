a9d29b9fd1d3453f69b43bf2fb5cbd6a4900442d
bey <betenatefera@gmail.com>
2021-12-08 00:01:39 +0300

    task 3 pushed

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Define class Square """
+
+
+def __init__(self, size=0):
+    """ Constructor """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    else:
+        self.__size = size
+def area(self):
+    return (self.__size ** 2)
