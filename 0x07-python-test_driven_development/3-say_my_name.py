93cfc13752a508339c95c994945382f1804e0d06
bey <betenatefera@gmail.com>
2021-12-12 15:33:14 +0300

    prompt to input is removed

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,4 @@
-def say_my_name(first_name, last_name=""):
-    first_name = str(input("enter your first name: "))
-    last_name = str(input("enter your last name: "))
+def say_my_name(first_name, last_name=""): 
     if not isinstance(first_name, str):
         raise TypeError("first name must be a string")
     if not isinstance(last_name, str):
