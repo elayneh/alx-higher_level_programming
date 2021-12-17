56119ecb2f93a266bce5d75831c5a2022861c707
bey <betenatefera@gmail.com>
2021-12-17 16:03:35 +0300

    parentesis added

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hashtable->size = size;
 	hashtable->array = malloc(sizeof(hash_node_t *) * size);
 	if (hashtable->array == NULL)
-		return NULL;
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 	{
