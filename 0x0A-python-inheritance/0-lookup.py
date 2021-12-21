f512ae806ed102b6a02cd4c6e939176c6f5f3965
bey <betenatefera@gmail.com>
2021-12-21 21:49:03 +0300

    task one pushed

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" attributes and methods of an object """
+
+def lookup(obj):
+    """ returns the list of variable attributes and methods of an object """
+    return(dir(obj))
