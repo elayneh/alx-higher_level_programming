5a34f111c1d695af21b9fe2dac64145b3716a151
b <betenatefera@gmail.com>
2022-03-17 14:44:55 -0400

    fixe error

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # script that takes a URL, sends to that URL and displays the size of the body of the response
 
-curl -sI GET "$1" | grep -i "Content-Length" cut -d " " -f2
+curl -sI "$1" | grep "Content-Length" | cut -d ' ' -f 2
