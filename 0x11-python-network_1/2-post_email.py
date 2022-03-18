2b531308a3c05ff2638b1c67c121fcd90b382319
b <betenatefera@gmail.com>
2022-03-18 16:36:56 -0400

    task added

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""Send POST and display the response"""
+"""Send POST"""
 import urllib.request
+import urllib.parse
 import sys
 
 
-def poster():
-    url = sys.argv[1]
-    email = {"email": sys.argv[2]}
-    request = urllib.request.Request(url, urllib.parse.urlencode(email))
-    with urllib.request.urlopen(request) as response:
-        page = response.read()
-        print(page.decode("utf-8"))
+def sender():
+    """sender"""
+    v = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(v)
+    data = data.encode("ascii")
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print(html.decode("utf-8"))
 
 if __name__ == "__main__":
-    poster()
+    sender()
