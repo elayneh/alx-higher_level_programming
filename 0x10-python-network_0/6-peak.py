7f9714d1637f1f0030686169c34e7346b07362b2
b <betenatefera@gmail.com>
2022-03-17 22:09:46 -0400

    test

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-""" Finds a peak in a list of unsorted integers """
+""" Finds a peak in a list of unsorted integers
+"""
 
 def find_peak(list_of_integers):
 
     size = len(list_of_integers)
-    recursive_mid = size
+    mid_e = size
     mid = size // 2
 
     if size == 0:
         return None
 
     while True:
-        recursive_mid = recursive_mid // 2
+        mid_e = mid_e // 2
         if (mid < size - 1 and
                 list_of_integers[mid] < list_of_integers[mid + 1]):
-            if recursive_mid // 2 == 0:
-                recursive_mid = 2
-            mid = mid + recursive_mid // 2
-        elif recursive_mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-            if recursive_mid // 2 == 0:
-                recursive_mid = 2
-            mid = mid - recursive_mid // 2
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid + mid_e // 2
+        elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid - mid_e // 2
         else:
             return list_of_integers[mid]
