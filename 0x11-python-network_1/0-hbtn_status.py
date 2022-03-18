26577bf396bddab383ac7e5578fb85aafdd01d3d
b <betenatefera@gmail.com>
2022-03-18 15:30:22 -0400

    task added

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import urllib.request
+def fun_fetch():
+    with urllib.request.urlopen("https://alx-interanet.hbtn.io/status") as response:
+        page = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(page)))
+        print("\t- content: {}".format(page))
+        print("\t- utf8 content: {}".format(page.decode("utf-8")))
+if __name__ == "__main__":
+    fun_fetch()
