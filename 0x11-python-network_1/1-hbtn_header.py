0e86472d942781575bd1f66b0185c0d1106e2fdd
b <betenatefera@gmail.com>
2022-03-18 16:10:53 -0400

    test

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,10 +2,11 @@
 """ take the url and send request to the url finally display the value of the X-Request-Id """
 import urllib.request, sys
 
-def fetch_header():
+def fetcher():
+    """function to fetch"""
     with urllib.request.urlopen(sys.argv[1]) as response:
         header = response.info()
         print(header["X-Request-Id"])
 
 if __name__ == "__main__":
-    fetch_header()
+    fetcher()
