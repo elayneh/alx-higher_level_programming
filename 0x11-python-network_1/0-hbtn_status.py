0c13af2dd303fa3d2fdf912ff92326d8e99965e0
b <betenatefera@gmail.com>
2022-03-18 15:40:47 -0400

    some modification

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
 import urllib.request
+
 def fun_fetch():
-    request = urllib.request.Request('https://intranet.hbtn.io/status')
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
     with urllib.request.urlopen(request) as response:
         page = response.read()
-        status = page.decode('utf-8')
-        print('Body response:')
-        print('\t- type: {}'.format(type(page)))
-        print('\t- content: {}'.format(page))
-        print('\t- utf8 content: {}'.format(status))
+        status = page.decode("utf-8")
+        print("Body response:")
+        print("\t- type: {}".format(type(page)))
+        print("\t- content: {}".format(page))
+        print("\t- utf8 content: {}".format(status))
 if __name__ == "__main__":
     fun_fetch()
