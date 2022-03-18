4c81209904d1af0ec1f58198eace6ab132e27a93
b <betenatefera@gmail.com>
2022-03-17 21:20:00 -0400

    task added

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!bin.bash
 # this script is to post request and display the response
-curl -sd email="test@gmail.com"&subject="I will always be here for PLD"
+curl -sd email="test@gmail.com"&subject="I will always be here for PLD" "$1"
