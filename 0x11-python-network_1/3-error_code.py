89174f06acb3aecf2906a2edc4e261b818ff36ed
b <betenatefera@gmail.com>
2022-03-18 22:02:40 -0400

    remove function

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,14 +4,10 @@ import urllib.request
 import urllib.error
 import sys
 
-def error_handler():
-    request = urllib.request.Request(sys.argv[1])
-    try:
-        with urllib.request.urlopen(request) as response:
-            response = response.decode("ascii")
-            print(response.read())
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
-
-if __name__ == "__main__":
-    error_handler()
+request = urllib.request.Request(sys.argv[1])
+try:
+    with urllib.request.urlopen(request) as response:
+        response = response.decode("ascii")
+        print(response.read())
+except urllib.error.HTTPError as e:
+    print("Error code: {}".format(e.code))
