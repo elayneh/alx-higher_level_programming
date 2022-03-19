88f01d9004f81361bb15f378c42eb87a2407bfd3
b <betenatefera@gmail.com>
2022-03-18 22:00:03 -0400

    remove function

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ import requests
 url = "https://alx-intranet.hbtn.io/status"
 out_put = requests.get(url)
 print("Body response:")
-print("\t- type: {}".format(type(out_put.text)))
-print("\t- content: {}".format(out_put.text))
+print("\t- type: {}".format(type(out_put)))
+print("\t- content: {}".format(out_put))
