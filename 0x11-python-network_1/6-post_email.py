9cd4231ba26299a8233ac141dcd3476412cc5cf2
b <betenatefera@gmail.com>
2022-03-18 23:17:40 -0400

    task added

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,8 +4,9 @@ finally display the response
 """
 import requests
 import sys
-url = sys.argv[1]
-value = {"email": sys.argv[2]
-         }
-response = requests.post(url, data=value)
-print(response.text)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]
+             }
+    response = requests.post(url, data=value)
+    print(response)
