fe5d9291443dcf98f56af51cb8db0190e4b1203a
b <betenatefera@gmail.com>
2022-03-18 21:57:28 -0400

    remove function

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,12 +3,8 @@
     """
 import requests
 
-
-def fetch():
-    url = "https://intranet.hbtn.io/status"
-    out_put = requests.get(url)
-    print("Body response:")
-    print("\t- type: {}".format(type(out_put.text)))
-    print("\t- content: {}".format(out_put.text))
-if __name__ == "__main__":
-    fetch()
+url = "https://intranet.hbtn.io/status"
+out_put = requests.get(url)
+print("Body response:")
+print("\t- type: {}".format(type(out_put.text)))
+print("\t- content: {}".format(out_put.text))
