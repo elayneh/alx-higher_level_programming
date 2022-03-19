80de0e81026533580476facd34c08d6083f7f76d
b <betenatefera@gmail.com>
2022-03-18 21:32:37 -0400

    modified

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 """fetch https://alx-interanet.hbtn.io/status
     """
-import sys
-import urllib.request
+import requests
 
 
 def fetch():
-    url = "https://alx-intranet.hbtn.io/status"
-    reqst = urllib.request.Request(url)
-    with urllib.request.urlopen(reqst) as resp:
-        out_put = resp.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(out_put)))
-        print("\t- content: {}".format(out_put))
+    url = "https://intranet.hbtn.io/status"
+    out_put = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(out_put.text)))
+    print("\t- content: {}".format(out_put.text))
 if __name__ == "__main__":
     fetch()
