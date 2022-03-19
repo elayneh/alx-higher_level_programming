8673e339b5c10574508ee81ca7ae000f5eee05bf
b <betenatefera@gmail.com>
2022-03-18 23:43:56 -0400

    task added

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,11 +4,11 @@
 import sys
 import requests
 
-if __name__=="__main__":
-    
+if __name__ == "__main__":
     url = sys.argv[1]
-    response = requests.get(url)
-    if(response.status_code >= 400):
-        print("Error code: {}".format(response.status_code))
+
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
     else:
-        print(response)
+        print(r.text)
