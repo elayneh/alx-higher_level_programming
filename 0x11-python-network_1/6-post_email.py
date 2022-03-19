afe13ad61143d4dca888acfe716b8399b9e3c7fe
b <betenatefera@gmail.com>
2022-03-18 23:29:07 -0400

    remove file format

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,7 +7,7 @@ import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-
+    value = {"email": sys.argv[2]
+             }
     r = requests.post(url, data=value)
     print(r)
