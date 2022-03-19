cd4351ca1a16cd133d83aecb6480348556c3df90
b <betenatefera@gmail.com>
2022-03-18 21:47:54 -0400

    test

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""take URL, sent request and display the value to the given
+    variable importing requests and sys only
+    """
+import requests
+import sys
+url = sys.argv[1]
+response = requests.get(url)
+print(response.headers.get("X-request-Id"))
