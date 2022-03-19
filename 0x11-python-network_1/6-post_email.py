fd541d62469eaba376fb241eededbd79cb61fe77
b <betenatefera@gmail.com>
2022-03-18 23:22:39 -0400

    test

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
+"""take url and an email address and post a request
+"""
 import sys
 import requests
 
@@ -9,4 +10,4 @@ if __name__ == "__main__":
     value = {"email": sys.argv[2]}
 
     r = requests.post(url, data=value)
-    print(r.text)
+    print(r.json)
