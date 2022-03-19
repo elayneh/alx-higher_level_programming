3def4613bfd55ede04acf44008bb11ff77b5ecf5
b <betenatefera@gmail.com>
2022-03-18 23:46:55 -0400

    further test

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     value = {"email": sys.argv[2]
              }
-    r = requests.post(url, data=value)
-    print(r)
+    response = requests.post(url, data=value)
+    print(response)
