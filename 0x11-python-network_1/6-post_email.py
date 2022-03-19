3c41a2b89da6e7accea7d17fce0504dd09525adb
b <betenatefera@gmail.com>
2022-03-18 23:18:54 -0400

    task added

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""take url and email address, send a post request
-finally display the response
-"""
-import requests
+
 import sys
+import requests
+
+
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email": sys.argv[2]
-             }
-    response = requests.post(url, data=value)
-    print(response)
+    value = {"email": sys.argv[2]}
+
+    r = requests.post(url, data=value)
+    print(r.text)
