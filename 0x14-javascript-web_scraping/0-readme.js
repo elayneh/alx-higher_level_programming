c7b54c460e5a2200693c45ab6f386b01bad9a1f7
elayneh <betenatefera@gmail.com>
2022-04-12 10:44:04 -0400

    modified

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 const fs = require("fs");
-fs.readFile(process.argv[2]. 'utf8', function(err, data) {
-	if (err){
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+	if (err) {
 		console.log(err);
 	}
-	else{
+	else {
 		process.stdout.write(data);
 	}
 });
