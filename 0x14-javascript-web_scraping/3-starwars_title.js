3b35c1c7ef68bcb28c2ba1f3bb3ed29a68ce7cac
elayneh <betenatefera@gmail.com>
2022-04-12 12:46:00 -0400

    modified

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,8 +2,8 @@
 
 request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]), function (_err, response, content) {
+request('https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]), function (_err, _response, content) {
 
 	content =JSON.parse(content);
 	console.log(content.title);
-})
+});
