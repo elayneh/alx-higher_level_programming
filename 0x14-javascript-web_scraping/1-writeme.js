e387b35fee2fd01ee301b6a4dea081770221b46f
elayneh <betenatefera@gmail.com>
2022-04-12 10:51:45 -0400

    modified

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
-const fs = require("fs");
-fs.write(process.argv[2], process.argv[3], 'utf8', function (err) {
-	if (err) {
-		console.log(err);
-	}
+
+const fs = require('fs');
+
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
+  if (err) {
+    console.log(err);
+  }
 });
