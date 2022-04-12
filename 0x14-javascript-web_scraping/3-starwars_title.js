be4ed093718c2bc03dfb8d5beb894b19267882c5
elayneh <betenatefera@gmail.com>
2022-04-12 11:36:23 -0400

    task added

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+request = require('request');
+
+request('https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]), function (_err, response, content) {
+
+	content =JSON.parse(content);
+	console.log(content.title);
+})
